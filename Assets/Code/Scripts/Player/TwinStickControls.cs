5af6633dd1db6b12cf889792dcb6a9cdcbc66ed7
SaSAsteroidBoss <conorcarr0@outlook.com>
2024-10-14 13:42:14 +0100

    Player Controls and Package Update

---
+++ Assets/Code/Scripts/Player/TwinStickControls.cs
@@ -0,0 +1,41 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TwinStickControls : MonoBehaviour
+{
+    CharacterController _controller;
+
+    Vector2 _moveInput;
+    
+    Vector2 _lookInput;
+
+    public float speed;
+
+   private void Start()
+   {
+       
+   }
+
+   private void Awake()
+    {
+        _controller = GetComponent<CharacterController>();
+    }
+
+    private void FixedUpdate()
+    {
+        MoveCharacter();
+    }
+
+
+    private void OnMove(InputValue value)
+    {
+        _moveInput = value.Get<Vector2>();
+    }
+    
+    private void MoveCharacter()
+    {
+        var movement = transform.right * _moveInput.x + transform.up * _moveInput.y;
+        _controller.Move(movement * (speed * Time.deltaTime));
+    }
+}
