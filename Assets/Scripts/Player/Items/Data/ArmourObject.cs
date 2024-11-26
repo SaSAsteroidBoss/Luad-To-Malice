7206b5160dfcd9ec5e674c1e9225e33bab905f3b
Conor <conorcarr0@outlook.com>
2024-11-26 09:36:05 +0000

    Armour

---
+++ Assets/Scripts/Player/Items/Data/ArmourObject.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+[CreateAssetMenu(menuName = "Item/Armour Item")]
+
+public class ArmourObject : ItemObject
+{
+    public float mainArmour;
+    
+    public void Awake()
+    {
+        type = ItemType.Armour;
+    }
+
+    public override void AddItemSource(GameObject player)
+    { 
+        player.GetComponent<Armour>().AddArmourSource(this);
+    }
+}
--- Assets/Scripts/Player/Items/Data/DamageObject.cs
+++ Assets/Scripts/Player/Items/Data/DamageObject.cs
@@ -1,7 +1,4 @@
-using System;
-using Unity.VisualScripting.FullSerializer;
 using UnityEngine;
-using UnityEngine.Serialization;
 
 [CreateAssetMenu(menuName = "Item/Damage Item")]
 
